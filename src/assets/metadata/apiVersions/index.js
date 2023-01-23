891362c323536ab6ba3f7ef3385cdc69d166530f
viennem <maxime.vienne@outlook.com>
2023-01-23 17:57:00 +0100

    wip

--- src/assets/metadata/apiVersions/index.js
+++ src/assets/metadata/apiVersions/index.js
@@ -1,12 +1,16 @@
 import appsV1 from './apps_v1.json';
+import batchV1 from './batch_v1.json';
 import coreV1 from './core_v1.json';
 import networkingV1 from './networking_v1.json';
+import autoscalingV2 from './autoscaling_v2.json';
 import others from './others.json';
 
 export default {
   'apps/v1': appsV1,
+  'batch/v1': batchV1,
   'v1': coreV1,
   'networking.k8s.io/v1': networkingV1,
+  'autoscaling/v2': autoscalingV2,
   'others': others,
 }
 
