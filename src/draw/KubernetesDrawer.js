8862f36d2801b2216c99f50f6ffebf8c28f3fc00
Aboubakar Khan <aboubakar_khan@hotmail.fr>
2023-09-21 17:11:18 +0200

    Add Layout param for KubernetesDrawer

--- src/draw/KubernetesDrawer.js
+++ src/draw/KubernetesDrawer.js
@@ -4,14 +4,15 @@ import { DefaultDrawer } from 'leto-modelizer-plugin-core';
  * Class to draw Kubernetes components.
  */
 class KubernetesDrawer extends DefaultDrawer {
-  constructor(pluginData, resources, rootId, options) {
+  constructor(pluginData, resources, rootId, options, layout) {
     super(pluginData, resources, rootId, {
       ...options,
       minHeight: 80,
       minWidth: 110,
       margin: 10,
       padding: 20,
-    });
+    },
+    layout);
   }
 }
 
