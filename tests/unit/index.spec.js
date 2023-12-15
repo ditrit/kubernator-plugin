eada7f28d76b36b3eae2954c72ff9f3b534a806a
X221782 <maxime.vienne-ext@socgen.com>
2023-12-15 11:33:12 +0100

    Add tests for KubernetesPlugin

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -1,11 +1,7 @@
-import Plugin from 'src/index';
+import KubernetesPlugin from 'src/index';
 
 describe('Test index of project', () => {
-  it('Index should return all needed objects', () => {
-    expect(Plugin.PluginDrawer).not.toBeNull();
-    expect(Plugin.PluginMetadata).not.toBeNull();
-    expect(Plugin.PluginParser).not.toBeNull();
-    expect(Plugin.PluginRenderer).not.toBeNull();
-    expect(Plugin.resources).not.toBeNull();
+  it('Index should export KubernetesPlugin', () => {
+    expect(new KubernetesPlugin().constructor.name).toBe('KubernetesPlugin');
   });
 });
